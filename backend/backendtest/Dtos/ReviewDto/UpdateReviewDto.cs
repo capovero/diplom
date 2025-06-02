bca87f42f0a71698e1067301065d03ebb7335945
capovero <xexevheburator@gmail.com>
2025-06-02 16:49:06 +0300

    project card + profile

--- backend/backendtest/Dtos/ReviewDto/UpdateReviewDto.cs
+++ backend/backendtest/Dtos/ReviewDto/UpdateReviewDto.cs
@@ -7,4 +7,4 @@ public class UpdateReviewDto
     [Range(0, 5, ErrorMessage = "Rating must be between 0 and 5.")]
     public int Rating { get; set; } // Оценка от 0 до 5
     public string Comment { get; set; } = string.Empty; // Текст отзыва
-}
+} 
